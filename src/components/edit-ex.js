188d48caa7667364abe2372dbe449f9589f5eb42
mayfielc <christopher.mayfield72@gmail.com>
2019-10-10 03:00:21 -0500

    components added on front end

---
+++ src/components/edit-ex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>Edit your workout list!!</p>
+            </div>
+        )
+    }
+}
+
+export default EditExercises;
---
+++ src/components/ex-list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Time to list your workouts!!</p>
+            </div>
+        )
+    }
+}
+
+export default ExercisesList;
